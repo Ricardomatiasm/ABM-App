ab99561aca3234b131225f317e9a51bbf880fee5
Ivanvillan <ivanvillan54@gmail.com>
2018-12-21 11:46:14 -0300

    ksadja

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './src/App/app';
-const App = document.getElementById('app');
+const app =  document.getElementById('app')
 
-render(<App />, App );
+
+render(<App />, app );
--- src/App/app.js
+++ src/App/app.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class App extends Component{
     render(){
         return(
-            <h1>HOLA</h1>
+            <div>
+                <h1>HOLA</h1>
+            </div>
         )
     }
 }
