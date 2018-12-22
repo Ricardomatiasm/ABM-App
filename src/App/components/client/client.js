1e0db93a6a1ba7057af6ace477153b0741af8bc2
MatiasRicardoM <matias.ricardo.m@gmail.com>
2018-12-22 09:36:06 -0300

    clientecito

---
+++ src/App/components/client/client.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './client.css'
+
+class Client extends Component {
+    render() {
+        return(
+            <h1>Componentecito de clientes!!</h1>
+        )
+    }
+ }
