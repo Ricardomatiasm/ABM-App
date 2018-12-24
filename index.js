b37f925284625fd406d91d3154a383b1982f4f78
Ivanvillan <ivanvillan54@gmail.com>
2018-12-24 01:14:11 -0300

    Rutas!!!!!!!!!!!!!!!!!!!!!!!!!

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './src/App/app';
+import { BrowserRouter as Router} from 'react-router-dom';
+
 const app =  document.getElementById('app')
 
 
-render(<App />, app );
+render(<Router>
+            <App />
+        </Router>, 
+        app);
